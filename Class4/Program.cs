94f2eec9c2d0b0176600ee29ef15d70b40fa84bb
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-02-07 19:25:38 +0200

    class 4

---
+++ Class4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Class4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input X:");
+                var isValidX = int.TryParse(Console.ReadLine(), out var x);
+                if (!isValidX)
+                {
+                    Console.WriteLine("Invalid input X!");
+                    break;
+                }
+
+                Console.WriteLine("Input Y:");
+                var isValidY = int.TryParse(Console.ReadLine(), out var y);
+                if (!isValidY)
+                {
+                    Console.WriteLine("Invalid input Y!");
+                    break;
+                }
+
+                var a = Math.Min(x, y);
+                var b = Math.Max(x, y);
+
+                var result = 0;
+
+                for (var temp = a; temp <= b; temp++)
+                {
+                    result += temp;
+                }
+
+                Console.WriteLine($"\nSum x...y: {result}");
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
